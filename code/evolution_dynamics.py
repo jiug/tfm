90ca89d2aa93f9652e58c09ec69d449e5c093474
jiug <hbeltranh@protonmail.com>
2026-03-27 12:02:36 +0800

    minor fixes better evolution metrics

--- code/evolution_dynamics.py
+++ code/evolution_dynamics.py
@@ -6,17 +6,16 @@ import assembly_pool as ap
 
 def main():
 
-    n_total = np.array([10, 50, 100, 500, 1000])
-    steps_total = 4 * n_total
-    evol_total = [[0, 0, 0, 0, 0] for _ in range(5)]
-    print(evol_total)
-    for i in range(len(n_total)):
-        n = n_total[i]
-        for j in range(len(steps_total)):
-            steps = steps_total[j]
+    n_total = np.array([50, 100, 500, 1000, 1500])
+    copies = 10
+    steps = 8000
+    evol_total = [[0, 0, 0, 0, 0] for _ in range(copies)]
+    for c in range(copies):
+        for i in range(len(n_total)):
+            n = n_total[i]
+            print(f"N: {n_total[i]},  {steps} steps")
             run = ap.AssemblyPool(n / 2, n / 2, 500)
-            print(i, j)
-            evol_total[i][j] = {f"{i}-{j}": run.evolve(steps)}
+            evol_total[c][i] = {f"{i}": run.evolve(steps)}
     return evol_total
 
 
